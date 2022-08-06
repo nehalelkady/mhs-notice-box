c5b829429d7cb73b81c2f4a59040ec0834f575b9
nehalelkady <nehal.elkady@gmail.com>
2022-08-06 10:24:45 -0700

    Delete mhs-notice-box directory

--- mhs-notice-box/mhs-notice-box.js
+++
@@ -1,7 +0,0 @@
-// MHS Notice Box jQuery
-jQuery(document).ready(function($) {
-	// Set up close button functionality
-	$('.mhs-notice-close').on('click', function(){
-			$(this).parents('.mhs-notice-box').remove();
-	});
-});
